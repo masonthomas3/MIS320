862538dccdd2d999be1b78c3ce8c7a3c3cec5ae8
masonthomas3 <mthomas3@iastate.edu>
2021-11-08 17:08:22 -0600

    Project structure additions

--- Doordash/doordash_webapp.py
+++ Doordash/doordash_webapp.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+WEB_APP_NAME = "MIS320"
 
 @app.route('/')
-def home():
-    return render_template("home.html")
+@app.route('/home')
+def home(name=WEB_APP_NAME):
+    return render_template("home.html", content=name)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
